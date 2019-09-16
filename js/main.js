7d0cb23cf504ad6e06b0fe25246b3304c9d19a8e
lokman <lokman@MacBook-Air-van-lokman.local>
2019-09-16 15:36:53 +0200

    foody project for hackyourfuture

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+//type code
+var typed = new Typed(".typed", {
+    strings: ["speak the good food language", 
+              
+],
+typeSpeed: 60,
+backSpeed: 60,
+loop: true
+  });
+
+  //aos 
+  AOS.init({
+      easing:'ease',
+      duration:1800
+  });
+
